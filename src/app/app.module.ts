ddbe629f33a8339c1a69967be5db3edb05c9acb1
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2023-03-19 20:01:20 -0300

    Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MenuComponent } from './components/menu/menu.component';
+import { CadastroSeguroComponent } from './components/cadastro-seguro/cadastro-seguro.component';
+import { ListarSeguroComponent } from './components/listar-seguro/listar-seguro.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CadastroSeguroComponent,
+    ListarSeguroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/cadastro-seguro/cadastro-seguro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-seguro',
+  templateUrl: './cadastro-seguro.component.html',
+  styleUrls: ['./cadastro-seguro.component.css']
+})
+export class CadastroSeguroComponent {
+
+}

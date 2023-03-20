d1adcae372972982a30744417c029ee01b6d1f2b
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2023-03-19 21:08:45 -0300

    insercaoModels

--- src/app/components/cadastro-seguro/cadastro-seguro.component.ts
+++ src/app/components/cadastro-seguro/cadastro-seguro.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { MarcaCarro } from 'src/app/models/MarcaCarro';
+import { Seguro } from 'src/app/models/Seguro';
+import { Observable } from 'rxjs';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro-seguro',
   templateUrl: './cadastro-seguro.component.html',
   styleUrls: ['./cadastro-seguro.component.css']
 })
-export class CadastroSeguroComponent {
+export class CadastroSeguroComponent implements OnInit {
+  public seguro = new Seguro();
+  public marcasCarros$: Observable<MarcaCarro[]>;
+
+  constructor{
+    private marcaCarroService: MarcaCarroService
+  } { }
+
+  ngOnInit(){
+    this.marcasCarro$ = this.marcaCarroService.getMarcas();
+  }
 
 }
